954a99b013043912864c3e3df207f40935dd9bf8
Binal Patel <147269633+Binalpatel1012@users.noreply.github.com>
2024-02-10 20:24:03 +0000

    Create a Post request that takes one argument of ToDoItem and creates a new ToDoItem, Create a Post request that takes one argument of Id and fills in the CompletedDate with the current datetime.

--- TodoList.API/Controllers/ToDoItemsController.cs
+++ TodoList.API/Controllers/ToDoItemsController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoList.Models;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace TodoList.API.Controllers
 {
@@ -36,6 +38,32 @@ namespace TodoList.API.Controllers
             }
 
             return toDoItem;
+        }
+
+        // POST: api/ToDoItems
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
+        {
+            _context.ToDoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetToDoItem), new { id = toDoItem.Id }, toDoItem);
+        }
+
+        // POST: api/ToDoItems/Complete/{id}
+        [HttpPost("Complete/{id}")]
+        public async Task<IActionResult> CompleteToDoItem(int id)
+        {
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+
+            toDoItem.CompletedDate = DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
